f6c00226a4190a2f708f70ef0fbfec95f6637fcb
Diego Barranco <diegobarranco92@gmail.com>
2018-05-24 10:07:45 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+require('./models/User');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+app.use(bodyParser.json());
+
+require('./routes/projectRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
