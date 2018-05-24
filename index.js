fd575ee5be4eee00858222475f61f65f1eb7e3bd
Diego Barranco <diegobarranco92@gmail.com>
2018-05-24 10:41:49 +0200

    Building Architecture

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 app.use(bodyParser.json());
 
-require('./routes/projectRoutes')(app);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
