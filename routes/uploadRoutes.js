9ebf2f1890f0408e1d68aa14b0d18578312a3f8d
Diego Barranco Moliner <diegobarranco92@gmail.com>
2018-08-08 02:03:40 +0200

    Upload Image Service in Heroku too

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -3,25 +3,17 @@ const uuid = require('uuid/v1');
 const requireLogin = require('../middlewares/requireLogin');
 const keys = require('../config/keys');
 
-const s3 = new AWS.S3({
-  accessKeyId: keys.accessKeyId,
-  secretAccessKey: keys.secretAccessKey,
-  region: 'eu-west-3'
-});
-
 AWS.config.update({
     accessKeyId: keys.accessKeyId,
     secretAccessKey: keys.secretAccessKey,
     region: 'eu-west-3'
 });
 
+const s3 = new AWS.S3();
 module.exports = app => {
   app.get('/api/upload', requireLogin, (req, res) => {
-    console.log(req.query)
     const key = `${req.query.folder}/${uuid()}.jpeg`;
-    console.log(req.query)
-    s3.getSignedUrl(
-      'putObject',
+    s3.putObject(
       {
         Bucket: 'iase-test',
         ContentType: 'image/jpeg',
