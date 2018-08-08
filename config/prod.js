98a1603f1efd04b7cfe2a5a777a7a393deb75e2b
Diego Barranco Moliner <diegobarranco92@gmail.com>
2018-08-08 02:09:12 +0200

    Upload Image Service in Heroku too

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,8 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY,
   key: process.env.KEY,
   sendGridUser: process.env.SENDGRID_USERNAME,
-  sendGridPass: process.env.SENDGRID_PASSWORD
+  sendGridPass: process.env.SENDGRID_PASSWORD,
+  accessKeyId: 'AKIAIPQBNLZJO4LCX5NQ',
+  secretAccessKey: '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'
 };
 //SENDGRID -> https://lairjgliargli.localtunnel.me/api/solicitud/webhook
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -3,27 +3,24 @@ const uuid = require('uuid/v1');
 const requireLogin = require('../middlewares/requireLogin');
 const keys = require('../config/keys');
 
-AWS.config.update({
-    accessKeyId: keys.accessKeyId,
-    secretAccessKey: keys.secretAccessKey,
-    region: 'eu-west-3'
+const s3 = new AWS.S3({
+  accessKeyId: keys.accessKeyId,
+  secretAccessKey: keys.secretAccessKey,
+  region: 'eu-west-3'
 });
 
-const s3 = new AWS.S3();
 module.exports = app => {
   app.get('/api/upload', requireLogin, (req, res) => {
     const key = `${req.query.folder}/${uuid()}.jpeg`;
-    s3.putObject(
+
+    s3.getSignedUrl(
+      'putObject',
       {
         Bucket: 'iase-test',
         ContentType: 'image/jpeg',
         Key: key
       },
-      (err, url) => {
-        console.log(err)
-        console.log(url)
-        res.send({ key, url })
-      }
+      (err, url) => res.send({ key, url })
     );
   });
 };
