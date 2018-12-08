5b63a1b06c56686f8f01b3295fcee4304a2bfe57
Diego Barranco Moliner <diegobarranco92@gmail.com>
2018-12-08 21:28:52 +0100

    https conf

--- index.js
+++ index.js
@@ -65,8 +65,8 @@ if (cluster.isMaster) {
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
-    const privateKey = fs.readFileSync('../../../../letsencrypt/live/www.iasegestion.com/privkey.pem');
-    const certificate = fs.readFileSync('../../../../letsencrypt/live/www.iasegestion.com/fullchain.pem');
+    const privateKey = fs.readFileSync('../../../../etc/letsencrypt/live/www.iasegestion.com/privkey.pem');
+    const certificate = fs.readFileSync('../../../../etc/letsencrypt/live/www.iasegestion.com/fullchain.pem');
 
     https.createServer({
         key: privateKey,
