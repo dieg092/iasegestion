e01bee352e9ed15c95297b7225f42828eef8b4db
Diego Barranco Moliner <diegobarranco92@gmail.com>
2018-12-08 23:14:59 +0100

    https conf

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
-  app.listen(9000);
+  // app.listen(9000);
 }
 
 module.exports = app;
