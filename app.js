e3dfb654974fd219c1d15ef0e0d7ba7004a2100a
Diego Barranco Moliner <diegobarranco92@gmail.com>
2018-12-08 22:41:42 +0100

    https conf

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+const compression = require('compression');
+const keys = require('./config/keys');
+require('dotenv').config();
+require('./models/Community');
+require('./models/Province');
+require('./models/Population');
+require('./models/Service');
+require('./models/Post');
+require('./models/Document');
+require('./models/User');
+require('./models/Token');
+require('./services/passport');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(compression());
+
+require('./routes/authRoutes')(app);
+require('./routes/userRoutes')(app);
+require('./routes/uploadRoutes')(app);
+require('./routes/serviceRoutes')(app);
+require('./routes/postRoutes')(app);
+require('./routes/documentRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognice the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
 const https = require('https');
-const flash = require('connect-flash');
+const http = require('http')
 const fs = require('fs');
-const compression = require('compression');
 const cluster = require('cluster');
 const os = require('os');
-const helmet = require('helmet');
-const keys = require('./config/keys');
-require('dotenv').config();
-require('./models/Community');
-require('./models/Province');
-require('./models/Population');
-require('./models/Service');
-require('./models/Post');
-require('./models/Document');
-require('./models/User');
-require('./models/Token');
-require('./services/passport');
+const app = require('./app');
 
 const numCPUs = require('os').cpus().length;
 
@@ -30,53 +13,19 @@ if (cluster.isMaster) {
     cluster.fork();
   }
 }  else {
-  mongoose.Promise = global.Promise;
-  mongoose.connect(keys.mongoURI);
-
-  const app = express();
-  app.use(helmet());
-  app.use(bodyParser.json());
-  app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(flash());
-  app.use(compression());
-
-  require('./routes/authRoutes')(app);
-  require('./routes/userRoutes')(app);
-  require('./routes/uploadRoutes')(app);
-  require('./routes/serviceRoutes')(app);
-  require('./routes/postRoutes')(app);
-  require('./routes/documentRoutes')(app);
-
-
   const PORT = process.env.PORT || 9000;
 
   if (process.env.NODE_ENV === 'production') {
-    // Express will serve up production assets
-    // like our main.js file, or main.css file!
-    app.use(express.static('client/build'));
-
-    // Express will serve up the index.html file
-    // if it doesn't recognice the route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
     const privateKey = fs.readFileSync('../../../../etc/letsencrypt/live/www.iasegestion.com/privkey.pem');
     const certificate = fs.readFileSync('../../../../etc/letsencrypt/live/www.iasegestion.com/fullchain.pem');
 
-    https.createServer({
+    const server = https.createServer({
         key: privateKey,
         cert: certificate
-    }, app).listen(PORT);
+    }, app);
 
   } else {
-    app.listen(PORT, 'localhost');
+    const server = http.createServer(app);
   }
+  server.listen(PORT);
 }
