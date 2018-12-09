b1bf4982d3090433dd6e716efadd4fbb50b51fbb
Diego Barranco Moliner <diegobarranco92@gmail.com>
2018-12-09 16:58:50 +0100

    listen

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
     // _id: Number,
 	  name: String,
     lastName: String,
-    nif: { type: String, unique: true, default: null },
+    nif: { type: String },
     email: { type: String, unique: true },
     password: String,
     isVerified: { type: Boolean, default: false },
