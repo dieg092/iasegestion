af83ae444b783f3d001dbe621a2587393bbd2be9
Diego Barranco <diegobarranco92@gmail.com>
2022-09-19 16:38:21 +0200

    bucket

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -9,6 +9,6 @@ module.exports = {
     'Informe': 'Informe'
   },
   URL: {
-    'photo' : !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? 'https://s3.eu-west-3.amazonaws.com/iase-test/' : 'https://s3.eu-west-3.amazonaws.com/iasegestion-bucket/'
+    'photo' : !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? 'https://s3.eu-west-3.amazonaws.com/iase-test/' : 'https://s3.eu-west-3.amazonaws.com/iasegestion/'
   }
 };
